f9ecff2deddb5c185a8ca3fabf180900ef17e745
Nicolas Pieuchot <n.pieuchot@zero-gachis.com>
2015-12-10 14:24:30 +0100

    Initial commit.

---
+++ datamax-bt-pc-test/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Test_BT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.btnAlive = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnCloseCOM = new System.Windows.Forms.Button();
+            this.btnOpenCOM = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.PortName = "COM23";
+            this.serialPort.ReadTimeout = 5000;
+            this.serialPort.WriteTimeout = 5000;
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // btnAlive
+            // 
+            this.btnAlive.Enabled = false;
+            this.btnAlive.Location = new System.Drawing.Point(191, 71);
+            this.btnAlive.Name = "btnAlive";
+            this.btnAlive.Size = new System.Drawing.Size(86, 32);
+            this.btnAlive.TabIndex = 0;
+            this.btnAlive.Text = "Alive ?";
+            this.btnAlive.UseVisualStyleBackColor = true;
+            this.btnAlive.Click += new System.EventHandler(this.btnAlive_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 109);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(265, 200);
+            this.txtLog.TabIndex = 1;
+            // 
+            // btnCloseCOM
+            // 
+            this.btnCloseCOM.Enabled = false;
+            this.btnCloseCOM.Location = new System.Drawing.Point(191, 315);
+            this.btnCloseCOM.Name = "btnCloseCOM";
+            this.btnCloseCOM.Size = new System.Drawing.Size(86, 32);
+            this.btnCloseCOM.TabIndex = 2;
+            this.btnCloseCOM.Text = "Close COM";
+            this.btnCloseCOM.UseVisualStyleBackColor = true;
+            this.btnCloseCOM.Click += new System.EventHandler(this.btnCloseCOM_Click);
+            // 
+            // btnOpenCOM
+            // 
+            this.btnOpenCOM.Location = new System.Drawing.Point(191, 33);
+            this.btnOpenCOM.Name = "btnOpenCOM";
+            this.btnOpenCOM.Size = new System.Drawing.Size(86, 32);
+            this.btnOpenCOM.TabIndex = 3;
+            this.btnOpenCOM.Text = "Open COM";
+            this.btnOpenCOM.UseVisualStyleBackColor = true;
+            this.btnOpenCOM.Click += new System.EventHandler(this.btnOpenCOM_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Step 1 : Select BT COM number ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(191, 7);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(86, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Step 2 : Open the connection";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Step 3 : Test if printer answers";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 325);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Step 4 : Close the connection";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 353);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpenCOM);
+            this.Controls.Add(this.btnCloseCOM);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnAlive);
+            this.Name = "Form1";
+            this.Text = "Datamax bluetooth test";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Button btnAlive;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnCloseCOM;
+        private System.Windows.Forms.Button btnOpenCOM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
